b7f93130a4094882df2aadaf15a134f59ef89dcc
leonardo <leonardo_bana@live.com.ar>
2020-01-16 18:34:03 -0300

    crud actualizado

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -13,12 +13,18 @@ const controller = {
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		
+		let product = products.find(function (p) {
+			return p.id == req.params.id
+		})
+		res
+        .cookie('product_ids', generateProductCookie(req, product))
+		.cookie('site', 'derive')
+		.render('detail', {product: product})
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		res.render('')
+		res.render('product-create-form')
 	},
 	
 	// Create -  Method to store
@@ -36,16 +42,40 @@ const controller = {
 		res.redirect('/')
 	},
 
-	// Update - Form to edit
+    // Update - Form to edit
 	edit: (req, res) => {
 		let product = products.find(function (p) {
 			return p.id == req.params.id
 		})
 
-		res.render()
+		res.render('product-edit-form', {
+			product: product
+		})
 	},
 
+	update: (req, res) => {
+		let arrayIndex
 
+		let product = products.find(function (p, index) {
+			if (p.id == req.params.id) {
+				arrayIndex = index
+				return true
+			}
+
+			return false
+		})
+
+		let editado = {
+			...product,
+			...req.body
+		}
+
+		products[arrayIndex] = editado
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products))
+
+		res.send('listo!')
+	},
 };
 
 module.exports = controller;
