8726747b7c1c5019ec341964ef725c3f339479d3
Vanesa Reyes <vanereyes22@gmail.com>
2020-01-29 21:58:55 -0300

    usando express validator para login y registro

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult, body } = require('express-validator');
+const fs = require('fs');
+
+let userValidation = [check('email').isEmail(), check('password').isLength({ min: 6, max: 12 }).withMessage('El password debe tener entre 6 y 12 caracteres')]
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.get('/login', function(req, res) {
+  res.render('register-form') //cambiar a login
+})
+
+router.get('/register', function(req, res) {
+    res.render('register-form')
+})
+
+router.post('/register', userValidation, function(req, res) {
+ 
+  let result = validationResult(req)
+
+  if (result.isEmpty()) {
+      return res.render('register-form', {
+              errors: result.errors,
+              data: req.body
+          }) 
+  }
+
+  let users = fs.readFileSync('../data/users.json', { encoding: 'utf-8' })
+  users = JSON.parse(users)
+  users.push(req.body)
+  users = JSON.stringify(users)
+  fs.writeFileSync('../data/users.json', users)
+
+  res.redirect(301, '/users/login')
+})
+
+router.post('/login', userValidation, function(req, res) {
+  let result = validationResult(req)
+
+  if (result.isEmpty()) {
+      return res.render('login', {
+              errors: result.errors,
+              data: req.body
+          }) 
+  }
+  res.redirect(301, '/users/welcome') //ver a donde redirigimos
+
+})
 
 module.exports = router;
