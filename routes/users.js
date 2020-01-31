88d78addf8f9ed93f7c4c13848bcca1cfceb87b7
Vanesa Reyes <vanereyes22@gmail.com>
2020-01-31 14:37:26 -0300

    agregue validacion usuario ya existente

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 let { check, validationResult, body } = require('express-validator');
 const usersController = require('../controllers/usersController');
+let usersFilePath = path.join(__dirname, '../data/users.json');
+let usersJSON = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 
 //let logDBMiddleware = require(‘../middlewares/lodDBMidleware’)
 
@@ -9,7 +14,21 @@ let userValidation = [
   check('name').isLength().withMessage('Este campo debe estar completo'), 
   check('surname').isLength().withMessage('Este campo debe estar completo'), 
   check('email').isEmail().withMessage('El email debe ser un email válido'), 
-  check('password').isLength({ min: 6, max: 12 }).withMessage('El password debe tener entre 6 y 12 caracteres')
+  check('password').isLength({ min: 6, max: 12 }).withMessage('El password debe tener entre 6 y 12 caracteres'),
+  body('email').custom(function(value){
+    let users;
+      if(usersJSON == "") {
+        users = [];
+      } else {
+        users = JSON.parse(users)
+      }
+      for (let i = 0; i < users.length; i ++) {
+        if (users[i].email == value) {
+          return false;
+        }
+      }
+      return true;
+  }).withMessage('Usuario ya existente')
 ]
 
 //router.get('/login', usersController.login);
