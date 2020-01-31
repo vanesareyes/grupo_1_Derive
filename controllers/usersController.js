800c0feabdaf012b4d97c77ba6d29b683acb6e81
Vanesa Reyes <vanereyes22@gmail.com>
2020-01-31 15:50:34 -0300

    metodo loginByPost y contraseña encriptada

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,46 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const { check, validationResult, body } = require('express-validator');
 let usersFilePath = path.join(__dirname, '../data/users.json');
-let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+let usersJSON = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const controller = {
     
     login: (req, res) => {
         res.send('formulario login') //cambiar a vista login
       },
-
-
-/*   function(req, res) {
-  let result = validationResult(req)
-
-  if (result.isEmpty()) {
-      return res.render('login', {
-              errors: result.errors,
-              data: req.body
-          }) 
-  }
-  //res.redirect(301, '/users/welcome') //ver a donde redirigimos
-
-}
-
-    loginByPost: (req, res) => {
-        let errors = validationResult(req)
-        if (!errors.isEmpty()){
-            res.render('login', {
-                errors: errors.errors,
-                data: req.body
-            })
-        }
-        req.session.user = {
-            id: 1,
-            email: 'john@doe.com'
-        }
-        
-        res.redirect(301, '/users/welcome')
-    },
-    welcomeUser: (req,res) => {
-        res.render('welcomeUser', {usuario: req.session.user})
-    }, */
     
     register: (req, res) => {      
         res.render('register-form')
@@ -53,18 +20,18 @@ const controller = {
         console.log(errors)
         if (errors.isEmpty()){
 
-            let usersJSON = fs.readFileSync('./data/users.json', { encoding: 'utf-8' })
             let users;
             if(usersJSON == "") {
                 users = [];
             } else {
-                users = JSON.parse(users)
+                users = JSON.parse(usersJSON)
             }
 
             let user = {
                 name: req.body.name,
                 surname: req.body.surname,
                 email: req.body.email,
+                //password: bcrypt.hashSync(req.body.password,10),
                 phone: req.body.phone,
             }
             users.push(user)
@@ -78,12 +45,45 @@ const controller = {
                     data: req.body
                      })
                 }
-} 
+    },
+    
+    loginByPost: (req, res) => {
+        let users;
+            if(usersJSON == "") {
+                users = [];
+            } else {
+                users = JSON.parse(usersJSON)
+            }
+
+      /*  for (let i = 0; i < users.length; i ++){
+            if(users[i].email == req.body.email && bcrypt.compareSync(req.body.password, users[i].password))
+            res.send('Welcome!')
+        }
+    */
+
+        let errors = validationResult(req)
+        if (!errors.isEmpty()){
+            res.render('login', {
+                errors: errors.errors,
+                data: req.body
+            })
+        }
+                
+        res.send('Welcome!')
+    },
     
 
 }
 
 
+
+/*    req.session.user = {
+            id: 1,
+            email: 'john@doe.com'
+        }
+    welcomeUser: (req,res) => {
+        res.render('welcomeUser', {usuario: req.session.user})
+    }, */
 
             
         /*res.send('estas logueado')
