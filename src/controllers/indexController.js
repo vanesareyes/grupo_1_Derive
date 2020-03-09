84c2fead8e97357f0812d526dbfa0e24d4848a04
Tadeo Angheben Martin <tadeoanghebenmartin@gmail.com>
2020-03-09 17:22:25 -0300

    creada-funcionalidad-busqueda

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -5,7 +5,8 @@ let faqs = JSON.parse(fs.readFileSync(faqsFilePath, 'utf-8'));
 //const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 //let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const db = require('../database/models');
-const sequelize = db.sequelize; 
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
 
 
 const controller = {
@@ -41,7 +42,35 @@ const controller = {
     },
 
     buscar: (req, res) => {
-        res.render('busqueda')
+        db.product.findAll({
+            where: {
+                    [Op.or]: [
+                      {
+                        name: {
+                          [Op.like]: '%'+req.query.busqueda+'%'
+                        }
+                      },
+                      {
+                        description: {
+                            [Op.like]: '%'+req.query.busqueda+'%'
+                        }
+                      }//,
+                        //{
+                        // location_id: {
+                        //     [Op.like]: '%'+req.params.busqueda+'%'
+                        //  }
+                        // }
+                    ]
+            },
+			include: [
+				"location",
+				"category"
+			]
+		}).then ((products) => {
+            res.render('busqueda', {
+                    products
+                })
+    })
     },
 
 };
